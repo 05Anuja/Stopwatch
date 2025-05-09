795ffb0be8cfc282930d1f0646a268ca5b74052e
AP1905 <anuja13122004@gmail.com>
2025-05-09 08:07:15 +0530

    changes

--- script.js
+++ script.js
@@ -8,24 +8,43 @@ let secs = 0;
 let mins = 0;
 let timerId = null;
 
-startBtn.addEventListener('click', function () {
+// startBtn.addEventListener('click', function () {
+//     if (timerId !== null) {
+//         clearInterval(timerId);
+//     }
+//     timerId = setInterval(startTimer, 10);
+// });
+
+function startBtnFun() {
     if (timerId !== null) {
         clearInterval(timerId);
     }
     timerId = setInterval(startTimer, 10);
-});
+}
 
-stopBtn.addEventListener('click', function() {
+// stopBtn.addEventListener('click', function() {
+//     clearInterval(timerId);
+// });
+
+function stopBtnFun() {
     clearInterval(timerId);
-});
+}
 
-resetBtn.addEventListener('click', function() {
+// resetBtn.addEventListener('click', function() {
+//     clearInterval(timerId);
+//     msecs = 0;
+//     secs = 0;
+//     mins = 0;
+//     timer.innerHTML = `00 : 00 : 00`
+// });
+
+function resetBtnFun() {
     clearInterval(timerId);
     msecs = 0;
     secs = 0;
     mins = 0;
-    timer.innerHTML = `00 : 00 : 00`
-})
+    timer.innerHTML = `00 : 00 : 00`;
+}
 
 function startTimer() {
     msecs ++;
